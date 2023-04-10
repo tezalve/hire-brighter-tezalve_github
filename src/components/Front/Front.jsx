8386e1db32d0be10989b5c3194e2d72065499539
tezalve <tezalve@gmail.com>
2023-04-10 15:32:23 +0600

    :sparkles: navbar & Front component done

--- src/components/Front/Front.jsx
+++ src/components/Front/Front.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Front.css'
+import { Button } from 'react-bootstrap';
 
 const Front = () => {
     return (
@@ -7,6 +8,7 @@ const Front = () => {
             <div>
                 <h1>Helping you build a better future, one job at a time</h1>
                 <p>Find your dream job with Hire Brighter - the ultimate job search destination. Discover top job listings, apply in just a few clicks, and get personalized career resources to thrive in your career.</p>
+                <Button>Get Started</Button>
             </div>
             <img src="../../public/assets/all_images/P3OLGJ1copy1.png" alt="" />
         </div>
